e3fdfb25061c014f73cfe56aa2b38b90397184bc
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2018-02-15 10:05:24 +0100

    Simple console application

---
+++ CSharpConsoleApp/NoteGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpConsoleApp {
+    internal class NoteGenerator {
+        public NoteGenerator() {
+            Console.Clear();
+            Console.WriteLine("Welcome to the note generator :D");
+            Console.WriteLine("To generate a note, type in the frequency and then the length in ms");
+            Console.WriteLine("The frequency has to be between 37 and 32767");
+            Console.WriteLine("<freq> <length>");
+            Console.WriteLine("Example: 6000 100");
+            Console.WriteLine("Return to the main menu by typing 'exit'");
+            NoteInput();
+        }
+
+        private void NoteInput() {
+            String stringInput = Console.ReadLine();
+            if (stringInput.ToLower().Equals("exit")) {
+                Program.greeting();
+                return;
+            }
+            String[] inputArray = stringInput.Split(' ');
+            try {
+                int freq = int.Parse(inputArray[0]);
+                int length = int.Parse(inputArray[1]);
+                consoleBeep(freq, length);
+            } catch (Exception) {
+                Console.WriteLine("Please enter a valid input");
+                NoteInput();
+            }
+        }
+
+        private void consoleBeep(int freq, int length) {
+            try {
+                Console.Beep(freq, length);
+            } catch (ArgumentOutOfRangeException) {
+                Console.WriteLine("frequency out of range");
+            } finally {
+                NoteInput();
+            }
+        }
+    }
+}
---
+++ CSharpConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            greeting();
+        }
+        public static void greeting() {
+            Console.Clear();
+            Console.WriteLine("Hi and welcome to this random console application. Please type a number for the function you wanna run ^^");
+            Console.WriteLine("1 - exit");
+            Console.WriteLine("2 - Note generator");
+            InpurHandler(ReadInput<int>());
+        }
+
+        private static void InpurHandler(int input) {
+            switch (input) {
+                case 1:
+                    Environment.Exit(0);
+                    break;
+                case 2:
+                    new NoteGenerator();
+                    break;
+                default:
+                    Console.WriteLine("Sorry, that program does not exist, try again ^^");
+                    InpurHandler(ReadInput<int>());
+                    break;
+            }
+        }
+
+        private static int ReadInput<int32>() {
+            
+            string userInput = Console.ReadLine();
+            int userInputInt;
+            try {
+                userInputInt = int.Parse(userInput);
+                return userInputInt;
+            }catch(Exception) {
+                return 0;
+            }
+        }
+
+        private static String ReadInput() {
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+    }
+}
